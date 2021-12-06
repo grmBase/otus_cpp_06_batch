6b61f7d0d868bfe9d1953a169c002abb8d75dd43
grmBase <grmBase@users.noreply.github.com>
2021-12-06 19:20:22 +0000

    deploy: 0927f91107e5f5e0bcd2fe92022d266e5dfc9cfa

--- search/all_7.js
+++ search/all_7.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['otus_5fcpp_5f06_5fbatch_0',['otus_cpp_06_batch',['../md_README.html',1,'']]]
+  ['otus_5fcpp_5f06_5fbulk_0',['otus_cpp_06_bulk',['../md_README.html',1,'']]]
 ];
